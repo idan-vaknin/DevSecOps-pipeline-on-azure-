007adba8b9a1abdf28090e3944f2d6dd74b68170
idan-vaknin <51876467+idan-vaknin@users.noreply.github.com>
2024-04-20 15:13:44 +0300

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -42,8 +42,8 @@ resource "azurerm_sql_server" "example" {
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = "exampleadmin"
-  administrator_login_password = "ChangePassword1!"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
   
   tags = {
     environment = "production"
